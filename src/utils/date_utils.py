51b51306369caaa50fa8a76b638c0753bca9fc7b
codexdive <codexdive@zhejianglab.com>
2024-04-02 14:18:46 +0800

    [MOD]根据要求修改功能函数的定义，添加注释

--- src/utils/date_utils.py
+++ src/utils/date_utils.py
@@ -11,25 +11,43 @@ from datetime import datetime
 class DateUtils:
     """日期时间的辅助类"""
 
+    """
+    将当前时刻转化为2024-04-01 22:32:13的格式
+    """
     @staticmethod
     def now_str():
         now = datetime.now()
         return now.strftime('%Y-%m-%d %H:%M:%S')
 
+    """
+    将当前时刻转化为2024-04-01 22:32:13-112345的格式，包好毫秒数
+    """
     @staticmethod
     def now_str_in_millis():
         now = datetime.now()
         return now.strftime('%Y-%m-%d-%H-%M-%S-%f')
 
+    """
+    将当前时刻转化为2024-04-01的格式
+    """
     @staticmethod
     def now_date():
         return datetime.now().strftime('%Y-%m-%d')
 
+
+    """
+    将传入的时刻转化为相应的毫秒数整数
+    """
     @staticmethod
     def timestamp_in_millis(now: datetime):
         print(datetime.timestamp(now))
         return int(datetime.timestamp(now) * 1000)
 
+
+    """
+    计算两个python 的datetime对象的间隔的毫秒数
+    """
+    
     @staticmethod
     def duration_in_millis(start_time: datetime, end_time: datetime):
         assert isinstance(start_time, datetime)
