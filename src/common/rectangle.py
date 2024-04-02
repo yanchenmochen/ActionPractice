2421215627a62013ac39b1b5badd383924f88dc2
codexdive <codexdive@zhejianglab.com>
2024-04-02 11:12:20 +0800

    [ADD]完成对于矩阵类功能的开发

---
+++ src/common/rectangle.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# rectangle.py
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+    def area(self):
+        return self.length * self.width
